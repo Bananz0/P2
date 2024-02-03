96bc6d802fd21c4be4c89337e8dfdfe78b3e31ab
Glen Muthoka <glenmuthokaq@gmail.com>
2024-02-03 01:59:48 +0000

    Changed classes for better functionality

--- P2/P2.cpp
+++ P2/P2.cpp
@@ -7,15 +7,28 @@ int main()
 {
     int num1, num2;
 
-    class singleBitAdder {
+    class BitAdder {
     public:
         int sum,carryOut;
-        int add(int a, int b, int carryIn) {
+
+        BitAdder(int a, int b, int carryIn) {
             sum = a ^ b ^ carryIn; 
+            //sum = (~a & b) | (~b & a);
             carryOut = (a & b) | (carryIn & (a ^ b));
+        }
+
+        int getSum() {
             return sum;
         }
+        int getCarry() {
+            return carryOut;
+        }
+        
     };
+
+   
+
+
 
     //User Input
     std::cout << "Please enter the first binary number (either 0 or 1): ";
@@ -23,11 +36,13 @@ int main()
     std::cout << "Please enter the second binary number (either 0 or 1): ";
     std::cin >> num2;
     
+    //Convert user input to bits
+
 
     //Actual adding
-    singleBitAdder adderUno;
-    std::cout << "Sum value is: " << adderUno.add(num1, num2, 0) << std::endl;
-    std::cout << "Carry value is " << adderUno.carryOut << std::endl;
+    BitAdder adderUno(1,1,0);
+    std::cout << "Sum value is: " << adderUno.getSum() << std::endl;
+    std::cout << "Carry value is " << adderUno.getCarry() << std::endl;
 
 
 }
