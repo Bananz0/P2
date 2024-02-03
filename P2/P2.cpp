0b567fcd6c4449657f06b5e6cff6a0990c79801d
Glen Muthoka <glenmuthokaq@gmail.com>
2024-02-03 04:47:38 +0000

    Implemented the nBitAdder but cannot find a way to display the array

--- P2/P2.cpp
+++ P2/P2.cpp
@@ -5,38 +5,77 @@
 
 int main()
 {
-    int num1, num2;
+    int input1, input2;
+    
 
+
+
+    //Single Bit Adder
     class BitAdder {
     public:
-        int sum,carryOut;
+        bool sum,carryOut;
 
-        BitAdder(int a, int b, int carryIn) {
+        BitAdder(bool a, bool b, int carryIn) {
             sum = a ^ b ^ carryIn; 
             //sum = (~a & b) | (~b & a);
             carryOut = (a & b) | (carryIn & (a ^ b));
         }
 
-        int getSum() {
+        bool getSum() {
             return sum;
         }
-        int getCarry() {
+        bool getCarry() {
             return carryOut;
         }
         
     };
 
+
+    //N - Bit adder
+    class nBitAdder {
+
+    private: 
+        bool sum, carry;
+        int totalSum[8];
+
+    public:
+        nBitAdder ()
+        {
+            int num1[8] = { 0,0,0,0,0,0 };
+
+            int num2[8] = { 0,0,0,0,0,0 };
+            bool carry = false;
+            for (int h = 0; h < 8; h++) {                
+                BitAdder adderUno(num1[h], num2[h], carry);
+                carry = adderUno.getCarry();
+
+                std::cout << "---------------: " << std::endl;
+                std::cout << "Sum value is: " << adderUno.getSum() << std::endl;
+                std::cout << "Carry value is " << adderUno.getCarry() << std::endl;
+                std::cout << "---------------: " << std::endl;
+                std::cout << ""  << std::endl;
+
+
+                totalSum[h] = adderUno.getSum();
+
+                
+            }
+        }
+
+        int getSum() {
+            return totalSum;
+        }
+    };
    
 
 
 
     //User Input
     std::cout << "Please enter the first binary number (either 0 or 1): ";
-    std::cin >> num1;
+    std::cin >> input1;
     std::cout << "Please enter the second binary number (either 0 or 1): ";
-    std::cin >> num2;
+    std::cin >> input2;
     
-    //Convert user input to bits
 
 
     //Actual adding
@@ -44,5 +83,22 @@ int main()
     std::cout << "Sum value is: " << adderUno.getSum() << std::endl;
     std::cout << "Carry value is " << adderUno.getCarry() << std::endl;
 
+    //nBit adding
+    nBitAdder fullAdder;
+    std::cout << "Total sum value is: " << fullAdder.getSum() << std::endl;
+ 
 
+}
+
+//Convert user input to bits
+int convertDecToBinary(int g)
+{
+    int binArrayMirror[16], bitSize = 0;
+
+    for (int i = 0; g > 0; i++) {
+        binArrayMirror[i] = g % 2;
+        g = g / 2;
+        bitSize += 1;
+    }
+    return 0;
 }
