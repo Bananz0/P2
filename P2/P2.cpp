d9c0d1202a39c9b97712f21c3fc1ddf7b7bc8fbc
Glen Muthoka <glenmuthokaq@gmail.com>
2024-02-06 07:51:40 +0000

    fixed a ton of stuff

--- P2/P2.cpp
+++ P2/P2.cpp
@@ -4,33 +4,43 @@
 #include <algorithm>
 
 
-std::vector<int> convertDecToBinary(int g);
-int convertBinaryToDec(std::vector<int> binaryArray);
+std::vector<bool> convertDecToBinary(int g);
 
-void printBinaryArray(std::vector<int> binaryArray);
+
+int convertBinaryToDec(std::vector<bool> binaryArray);
+std::vector<bool> complementBinaryArray(std::vector<bool> binaryArray);
+void printBinaryArray(std::vector<bool> binaryArray);
+
+
+
+int input1, input2;
+char operatorType;
+int maxSize = 8;
+
+
+
 //This is an attempt to build a binary adder
 
 int main()
 {
-    int input1, input2;
-    char operatorType;
-
+    
     //Single Bit Adder
     class BitAdder {
     public:
-        int sum, carryOut;
+        bool sum = 0; 
+        bool carryOut = 0;
 
-        BitAdder(int a, int b, int carryIn) {
+        BitAdder(bool a, bool b, bool carryIn) {
             carryOut = 0;
             sum = a ^ b ^ carryIn;
             //sum = (~a & b) | (~b & a);
             carryOut = (a & b) | (carryIn & a) | (carryIn & b);
         }
 
-        int getSum() {
+        bool getSum() {
             return sum;
         }
-        int getCarry() {
+        bool getCarry() {
             return carryOut;
         }
 
@@ -40,18 +50,27 @@ int main()
     class nBitAdder {
 
     private:
-        int sum, carry;
-        std::vector<int> totalSum, totalSumReverse;
+        bool sum, carry;
+        std::vector<bool> totalSum;
 
     public:
-        nBitAdder(std::vector<int> num1, std::vector<int> num2)
+        nBitAdder(std::vector<bool> num1, std::vector<bool> num2)
         {
-            int maxSize = std::max(num1.size(), num2.size());
-            int carry = false;
-            for (int h = 0; h < maxSize; h++) {
-                int bit1 = h < num1.size() ? num1[h] : 0;
-                int bit2 = h < num2.size() ? num2[h] : 0;
-                BitAdder adderUno(bit1, bit2, carry);
+            //int maxSize = std::max(num1.size(), num2.size());
+            //int minSize = std::min(num1.size(), num2.size());
+            //maxSize = 9;
+
+
+            bool carry = false;
+            for (int h = maxSize - 1; h > 0; h--) {
+
+
+
+
+                //bool bit1 = h < num1.size() ? num1[h] : 0;
+                //bool bit2 = h < num2.size() ? num2[h] : 0;
+                BitAdder adderUno(num1[h], num2[h], carry);
+
 
                 //std::cout << "" << std::endl;
                 //std::cout << "---------------- " << std::endl;
@@ -59,77 +78,75 @@ int main()
                 //std::cout << "Carry value is " << adderUno.getCarry() << std::endl;
                 //std::cout << "---------------- " << std::endl;
                 //std::cout << ""  << std::endl;
-
-
+    
                 totalSum.push_back(adderUno.getSum());
+
                 /*std::cout << totalSum[h];*/
                 carry = adderUno.getCarry();
-                if (h == maxSize - 1) {
-                    totalSum.push_back(adderUno.getCarry());
-                }
-                totalSumReverse = totalSum;
-                std::reverse(totalSumReverse.begin(), totalSumReverse.end());
                 //printBinaryArray(totalSum);
-                //printBinaryArray(totalSumReverse);
-
             }
-
         }
-
-        std::vector<int> getSum() {
-            return totalSumReverse;
+        std::vector<bool> getSum() {
+            return totalSum;
         }
     };
 
     //User Input
     std::cout << "Please enter the first binary number (A): ";
     std::cin >> input1;
-    std::vector<int> binary1 = convertDecToBinary(input1);
-
-    std::cout << "Please enter the second binary number (B): ";
-    std::cin >> input2;
-    std::vector<int> binary2 = convertDecToBinary(input2);
+    std::vector<bool> binary1 = convertDecToBinary(input1);
 
     std::cout << "Select Operation (+ or -): ";
     std::cin >> operatorType;
-    
 
-
-    //Operator checking
-
+    std::cout << "Please enter the second binary number (B): ";
+    std::cin >> input2;
+    std::vector<bool> binary2 = convertDecToBinary(input2);
 
     nBitAdder fullAdder(binary1, binary2);
 
-    //int decTotal = convertBinaryToDec({1,0,1,1,1,0,1});
+    //int decTotal = convertBinaryToDec({1,1,0});
     int decTotal = convertBinaryToDec(fullAdder.getSum());
 
     printBinaryArray(fullAdder.getSum());
     std::cout << "The decimal equivalent is: " << decTotal;
-
-
-
-
 }
 
-//Convert user input to bits
-std::vector<int> convertDecToBinary(int g)
+
+
+std::vector<bool> convertDecToBinary(int g)
 {
-    std::vector<int> binArray, binArrayOut;
-    while (g > 0) {
+    std::vector<bool> binArray, binArrayOut, binArrayComplement;
+    
+    if (g < 0) {
+        g = -g;
+        operatorType = '-';
+    }
+
+    while (g != 0) {
         binArray.push_back(g % 2);
         g = g / 2;
     }
-    binArrayOut = binArray;
-    std::reverse(binArrayOut.begin(), binArrayOut.end());
-    std::cout << "The corresponding value in binary is: ";
-    for (int y = 0; y < binArrayOut.size(); y++) {
-        std::cout << binArrayOut[y];
+    std::reverse(binArray.begin(), binArray.end());
+    while (binArray.size() < maxSize) {
+        binArray.insert(binArray.begin(), false); // Insert '0' at the beginning
     }
-    std::cout << std::endl;
-    return binArray;
+
+
+    binArrayOut = binArray;
+    if (operatorType == '-') {
+    binArrayComplement = complementBinaryArray(binArray);
+    binArrayOut = binArrayComplement;
+        
+    } else if (operatorType == '+'){
+        binArrayOut = binArray;
+    }
+    printBinaryArray(binArrayOut);
+    return binArrayOut;
+
 }
 
-int convertBinaryToDec(std::vector<int> binaryArray) {
+int convertBinaryToDec(std::vector<bool> binaryArray) {
     int decNum = 0;
     int n = binaryArray.size();
     //std::cout << "The size of the binary array is: " << n << std::endl;
@@ -139,9 +156,28 @@ int convertBinaryToDec(std::vector<int> binaryArray) {
     return decNum;
 }
 
-void printBinaryArray(std::vector<int> binaryArray) {
-    for (int y = 0; y < binaryArray.size(); y++) {
-        std::cout << binaryArray[y];
+void printBinaryArray(std::vector<bool> binArray) {
+    std::cout << "The corresponding value in binary is: ";
+    for (int y = 0; y < binArray.size(); y++) {
+        std::cout << binArray[y];
     }
     std::cout << std::endl;
+}
+
+std::vector<bool> complementBinaryArray(std::vector<bool> binaryArray) {
+    std::vector<bool> binArrayComplement;
+    //One's complement operation
+    for (int i = binaryArray.size() - 1; i > 0; i--) {
+        binArrayComplement.push_back(1 ^ binaryArray[i]);
+    }
+    //Two's complement operation
+    //Add 1
+    for (int j = binArrayComplement.size() - 1; j > 0; j--) {
+        if (binArrayComplement[j] == 0) {
+            binArrayComplement[j] = 1;
+            break;
+        }
+        else { binArrayComplement[j] = 0; }
+        }
+    return binArrayComplement;
 }
